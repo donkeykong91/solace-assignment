23a8722ddfc25fabdb5c03fd3709f96a88d9480f
donkeykong91 <darylnator@gmail.com>
2025-10-06 06:53:52 -0700

    💡Added notes to discussion file 🩹Updated to using shortcut symbol for imports

---
+++ DISCUSSION.md
@@ -0,0 +1,8 @@
+1. Wish I had time to do some more debugging of why I was not able to make a GET call to the database
+2. Did some debugging with Postman when making the GET call and did console.log of the response from GET call
+3. Wish had time to add friendly error modals or messages for search when putting in random text
+4. Wish had more time to filter for special character and char limits and put validation messages under field of what was needed to continue to click on search button
+5. Wish had more time for styling
+6. Wish had more time to add a loading animation with skeleton and other loading animations for each loading component during search
+7. Wish had more time to put the plaintext into their own files and pull from the file to also setup for content management
+8. Wish had more time to add error handling for specific API errors like 404, 401, 403, 500 and respond accordingly with user-friendly messages as well as leave codes for developers for easy debugging
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,7 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
-const setup = () => {
+export const setup = () => {
   if (!process.env.DATABASE_URL) {
     console.error("DATABASE_URL is not set");
     return {
@@ -11,10 +11,29 @@ const setup = () => {
     };
   }
 
-  // for query purposes
-  const queryClient = postgres(process.env.DATABASE_URL);
-  const db = drizzle(queryClient);
-  return db;
+    // for query purposes
+    const queryClient = postgres(process.env.DATABASE_URL);
+    const db = drizzle(queryClient);
+    return db;
 };
 
-export default setup();
+// const globalForDb = globalThis as unknown as {
+//     pg?: ReturnType<typeof postgres>;
+//     db?: ReturnType<typeof drizzle<typeof schema>>;
+// };
+
+// export const pg =
+//     globalForDb.pg ??
+//     postgres(process.env.DATABASE_URL as string, {
+//         max: 10,
+//         idle_timeout: 20,
+//         prepare: false,
+//     });
+
+// export const db =
+//     globalForDb.db ?? drizzle(pg, { schema });
+//
+// if (!globalForDb.pg) globalForDb.pg = pg;
+// if (!globalForDb.db) globalForDb.db = db;
+
+export default setup;
